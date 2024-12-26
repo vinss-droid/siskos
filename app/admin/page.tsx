45a967e4d8328d41d460774d2498c3489ffa7c86
vinss-droid <kevinsipahutar220604@gmail.com>
2024-12-26 20:57:22 +0700

    finish all features

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -14,7 +14,7 @@ import {
 import {IconPencil, IconPlus, IconTrash} from "@tabler/icons-react";
 import {createClient} from "@supabase/supabase-js";
 import React from "react";
-import {string} from "postcss-selector-parser";
+import {Bounce, toast} from "react-toastify";
 
 const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || '';
@@ -30,15 +30,33 @@ type Notes = {
 }
 
 interface NewNote {
-    nama: string,
-    kamarId: number,
+    nama?: string,
+    kamarId?: number,
+}
+
+interface DataKwh {
+    id?: number;
+    kamarId?: number | undefined;
+    penyewa?: string | undefined;
+    KWhBulanIni?: number | undefined;
+    KWhBulanLalu?: number | undefined;
+}
+
+interface DataKamar {
+    id?: number;
+    kamarId?: number | undefined;
+    penyewa?: string | undefined;
 }
 
 export default function AdminPage() {
 
     const modalAdd = useDisclosure();
+    const modalEdit = useDisclosure();
+    const modalDelete = useDisclosure();
     const [data, setData] = React.useState<Notes[] | null>()
     const [dataNew, setDataNew] = React.useState<NewNote>()
+    const [dataKWh, setDataKWh] = React.useState<DataKwh>()
+    const [dataKamar, setDataKamar] = React.useState<DataKamar>()
 
     const getNotes = async () => {
         const { data } = await supabase.from('catatan').select()
@@ -47,12 +65,107 @@ export default function AdminPage() {
 
     const addNotes = async () => {
         const {error} = await supabase.from('catatan')
-            .insert([{
+            .insert({
                 nama: dataNew?.nama,
                 kamarId: dataNew?.kamarId,
-            }])
+            })
 
-        if (error) throw error
+        if (error) toast.error('Error while inserting data.', {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: false,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            transition: Bounce,
+        });
+
+        toast.success('Berhasil menambahkan data.', {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: false,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            transition: Bounce,
+        });
+
+        getNotes();
+        modalAdd.onClose()
+    }
+
+    const updateKWh = async () => {
+        const id = dataKWh?.id ?? 0
+        const {error} = await supabase.from('catatan')
+            .update({
+                kwhBulanIni: dataKWh?.KWhBulanIni,
+                kwhBulanLalu: dataKWh?.KWhBulanLalu,
+            })
+            .eq('id', id)
+
+        if (error) toast.error('Error while updating data.', {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: false,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            transition: Bounce,
+        });
+
+        toast.success('Berhasil update KWh.', {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: false,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            transition: Bounce,
+        });
+
+        modalEdit.onClose()
+    }
+
+    const deleteKamar = async () => {
+        const id = dataKamar?.id ?? 0
+        const {error} = await supabase.from('catatan')
+            .delete()
+            .eq('id', id)
+
+        if (error) toast.error('Error while deleting data.', {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: false,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            transition: Bounce,
+        });
+
+        toast.success('Berhasil hapus data kamar.', {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: false,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            transition: Bounce,
+        });
+
+        getNotes()
+        modalDelete.onClose()
     }
 
     React.useEffect(() => {
@@ -81,10 +194,20 @@ export default function AdminPage() {
                                     <TableCell>{note.nama}</TableCell>
                                     <TableCell>{note.kamarId}</TableCell>
                                     <TableCell>
-                                        <Button isIconOnly color="warning" className="me-2">
+                                        <Button isIconOnly color="warning" className="me-2" onPress={() => {
+                                            setDataKWh({
+                                                ...dataKWh, id: note.id, kamarId: note.kamarId, penyewa: note.nama
+                                            })
+                                            modalEdit.onOpen()
+                                        }}>
                                             <IconPencil color="white" />
                                         </Button>
-                                        <Button isIconOnly color="danger">
+                                        <Button isIconOnly color="danger" onPress={() => {
+                                            setDataKamar({
+                                                ...dataKWh, id: note.id, kamarId: note.kamarId, penyewa: note.nama
+                                            })
+                                            modalDelete.onOpen()
+                                        }}>
                                             <IconTrash color="white" />
                                         </Button>
                                     </TableCell>
@@ -106,13 +229,19 @@ export default function AdminPage() {
             <ModalContent>
                 <ModalHeader className="flex flex-col gap-1">Tambah Data Kamar</ModalHeader>
                 <ModalBody>
-                    <form>
+                    <form onSubmit={(e) => {
+                        e.preventDefault()
+                        addNotes()
+                    }}>
                         <Input
                             isRequired
                             label="Nama Penyewa"
                             type="text"
                             required
                             className="mb-3"
+                            onChange={(e) => setDataNew({
+                                ...dataNew, nama: e.target.value
+                            })}
                         />
                         <Input
                             isRequired
@@ -120,11 +249,74 @@ export default function AdminPage() {
                             type="number"
                             required
                             className="mb-5"
+                            onChange={(e) => setDataNew({
+                                ...dataNew, kamarId: parseInt(e.target.value)
+                            })}
                         />
                         <Button color="primary" className="mb-4 w-full" type="submit">
                             Simpan
                         </Button>
                     </form>
+                </ModalBody>
+            </ModalContent>
+        </Modal>
+
+        {/*  Modal Edit  */}
+        <Modal
+            isDismissable={false}
+            isKeyboardDismissDisabled={true}
+            isOpen={modalEdit.isOpen}
+            onOpenChange={modalEdit.onOpenChange}
+        >
+            <ModalContent>
+                <ModalHeader className="flex flex-col gap-1">Update KWh Kamar | {`${dataKWh?.penyewa} Kamar No.${dataKWh?.kamarId}`}</ModalHeader>
+                <ModalBody>
+                    <form onSubmit={(e) => {
+                        e.preventDefault()
+                        updateKWh()
+                    }}>
+                        <Input
+                            isRequired
+                            label="KWh Bulan Lalu"
+                            type="number"
+                            required
+                            className="mb-3"
+                            onChange={(e) => setDataKWh({
+                                ...dataKWh, KWhBulanLalu: parseInt(e.target.value)
+                            })}
+                        />
+                        <Input
+                            isRequired
+                            label="KWh Bulan Ini"
+                            type="number"
+                            required
+                            className="mb-5"
+                            onChange={(e) => setDataKWh({
+                                ...dataKWh, KWhBulanIni: parseInt(e.target.value)
+                            })}
+                        />
+                        <Button color="primary" className="mb-4 w-full" type="submit">
+                            Simpan
+                        </Button>
+                    </form>
+                </ModalBody>
+            </ModalContent>
+        </Modal>
+
+
+        {/*  Modal Delete  */}
+        <Modal
+            isDismissable={false}
+            isKeyboardDismissDisabled={true}
+            isOpen={modalDelete.isOpen}
+            onOpenChange={modalDelete.onOpenChange}
+        >
+            <ModalContent>
+                <ModalHeader className="flex flex-col gap-1">Hapus Data | {`${dataKamar?.penyewa} Kamar No.${dataKamar?.kamarId}`}</ModalHeader>
+                <ModalBody>
+                    <Button color="danger" className="mb-4 w-full" type="button" onPress={deleteKamar}>
+                        Hapus
+                    </Button>
                 </ModalBody>
             </ModalContent>
         </Modal>
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import {Providers} from "@/app/providers";
+import {ToastContainer} from "react-toastify";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased my-12`}
       >
         <Providers>
-            {children}
+          <ToastContainer />
+          {children}
         </Providers>
       </body>
     </html>
